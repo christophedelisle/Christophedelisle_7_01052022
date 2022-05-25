b603960494530a104a07e2c4b6e9384b3f93432b
Christophedl <christophedelisle@hotmail.fr>
2022-05-25 16:06:36 +0200

    start frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      test
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header-container">
+        <h1>test header</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
